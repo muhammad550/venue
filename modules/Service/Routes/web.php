a51a43aeee0fe9a1b4ab881c57547c94e4e4154e
Muhammad Shahzad <shahzadm287@gmail.com>
2025-06-04 01:46:01 +0500

    delete serivce folder

--- modules/Service/Routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-//Contact
-Route::get('/service','ServiceController@index')->name("service.index");
-// Route::post('/contact/store','ContactController@store')->name("contact.store");
